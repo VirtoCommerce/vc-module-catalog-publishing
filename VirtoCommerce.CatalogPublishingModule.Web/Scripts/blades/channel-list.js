075b1bc463969315ca949cffa83a8436be5627ba
Andrew Orlov <ao@virtoway.com>
2017-04-18 17:06:13 +0200

    Fixed module failing on start

--- VirtoCommerce.CatalogPublishingModule.Web/Scripts/blades/channel-list.js
+++ VirtoCommerce.CatalogPublishingModule.Web/Scripts/blades/channel-list.js
@@ -1,15 +1,15 @@
-﻿//angular.module('virtoCommerce.catalogPublishingModule')
-//    .controller('virtoCommerce.catalogPublishingModule.channelListController', ['$scope', 'platformWebApp.uiGridHelper', function ($scope, uiGridHelper) {
-//        var blade = $scope.blade;
-//        blade.isLoading = false;
+﻿angular.module('virtoCommerce.catalogPublishingModule')
+    .controller('virtoCommerce.catalogPublishingModule.channelListController', ['$scope', 'platformWebApp.uiGridHelper', function ($scope, uiGridHelper) {
+        var blade = $scope.blade;
+        blade.isLoading = false;
 
-//        $scope.setGridOptions = function (gridOptions) {
-//            $scope.gridOptions = gridOptions;
-//            gridOptions.onRegisterApi = function (gridApi) {
-//                gridApi.core.on.sortChanged($scope, function () {
-//                    if (!blade.isLoading) blade.refresh();
-//                });
-//            };
-//            bladeUtils.initializePagination($scope);
-//        };
-//    }]);
+        $scope.setGridOptions = function (gridOptions) {
+            $scope.gridOptions = gridOptions;
+            gridOptions.onRegisterApi = function (gridApi) {
+                gridApi.core.on.sortChanged($scope, function () {
+                    if (!blade.isLoading) blade.refresh();
+                });
+            };
+            bladeUtils.initializePagination($scope);
+        };
+    }]);
