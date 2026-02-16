0366de9b9ff04e26def8522c0235bece199a469d
Oleg Zhuk <zhukoo@gmail.com>
2026-02-16 09:06:26 +0200

    VCST-4328: Update to .NET10 (#70)

---
+++ src/VirtoCommerce.CatalogPublishingModule.Data.SqlServer/SqlServerDataAssemblyMarker.cs
@@ -0,0 +1,3 @@
+namespace VirtoCommerce.CatalogPublishingModule.Data.SqlServer;
+
+public class SqlServerDataAssemblyMarker;
--- src/VirtoCommerce.CatalogPublishingModule.Web/Module.cs
+++ src/VirtoCommerce.CatalogPublishingModule.Web/Module.cs
@@ -18,6 +18,9 @@ using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Data.Extensions;
+using VirtoCommerce.Platform.Data.MySql.Extensions;
+using VirtoCommerce.Platform.Data.PostgreSql.Extensions;
+using VirtoCommerce.Platform.Data.SqlServer.Extensions;
 using VirtoCommerce.SearchModule.Core.Model;
 
 namespace VirtoCommerce.CatalogPublishingModule.Web
@@ -37,13 +40,13 @@ namespace VirtoCommerce.CatalogPublishingModule.Web
                 switch (databaseProvider)
                 {
                     case "MySql":
-                        options.UseMySqlDatabase(connectionString);
+                        options.UseMySqlDatabase(connectionString, typeof(MySqlDataAssemblyMarker), Configuration);
                         break;
                     case "PostgreSql":
-                        options.UsePostgreSqlDatabase(connectionString);
+                        options.UsePostgreSqlDatabase(connectionString, typeof(PostgreSqlDataAssemblyMarker), Configuration);
                         break;
                     default:
-                        options.UseSqlServerDatabase(connectionString);
+                        options.UseSqlServerDatabase(connectionString, typeof(SqlServerDataAssemblyMarker), Configuration);
                         break;
                 }
             });
