3e68817f4a697ee8bdfd0d8978d00c11aebfe8c0
Oleg Zhuk <zhukoo@gmail.com>
2026-04-07 12:51:36 +0200

    VCST-4880: The model context has pending changes for MySQL (#74)

--- src/VirtoCommerce.CatalogPublishingModule.Data.MySql/Migrations/20260406162913_FixPendingModelChangesWarning.Designer.cs
+++ src/VirtoCommerce.CatalogPublishingModule.Data.MySql/Migrations/20260406162913_FixPendingModelChangesWarning.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.CatalogPublishingModule.Data.Repositories;
 
@@ -11,9 +12,11 @@ using VirtoCommerce.CatalogPublishingModule.Data.Repositories;
 namespace VirtoCommerce.CatalogPublishingModule.Data.MySql.Migrations
 {
     [DbContext(typeof(CatalogPublishingDbContext))]
-    partial class CatalogPublishingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260406162913_FixPendingModelChangesWarning")]
+    partial class FixPendingModelChangesWarning
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
